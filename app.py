b104f38d6e2ae25b8a1685c441496cde725664cb
felipealfah <felipealfah@gmail.com>
2024-04-29 12:58:20 -0300

    V2.1

--- app.py
+++ app.py
@@ -11,26 +11,25 @@ URL = "https://cbsnooper.com/reports/top-clickbank-products"
 def index():
     return "<h1>App Ninja Click</h1>"
 
-def scrape_in_background(session, URL, max_pages, queue):
+def scrape_in_background(session, URL, queue):
     from scrape.scrape import parse_all_pages_requests
     try:
-        data, pages_processed = parse_all_pages_requests(session, URL, max_pages=max_pages)
+        data, pages_processed = parse_all_pages_requests(session, URL)
         print(f"Total de páginas processadas: {pages_processed}")
         print(f"Total de produtos extraídos: {len(data)}")
-        queue.put((data, pages_processed))  # Put the results in the queue
+        queue.put((data, pages_processed))
     except Exception as e:
         print(f"Erro durante o scraping: {e}")
-        queue.put((pd.DataFrame(), 0))  # Put empty result and 0 pages processed in case of exception
+        queue.put((pd.DataFrame(), 0))
 
 @app.route('/scrape', methods=['GET'])
 def scrape_data():
-    max_pages = request.args.get('max_pages', default=3, type=int)
     from login.login import login_to_cbsnooper_and_transfer_session
     
     session = login_to_cbsnooper_and_transfer_session()
     
     result_queue = Queue()
-    thread = Thread(target=scrape_in_background, args=(session, URL, max_pages, result_queue))
+    thread = Thread(target=scrape_in_background, args=(session, URL, result_queue))
     thread.start()
     thread.join()  # This will wait for the thread to complete
     
@@ -44,5 +43,3 @@ def scrape_data():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
-#v1.2
